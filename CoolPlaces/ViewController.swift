d77024f07d1599fbacc4e1b9965287f2e41fd722
Pablo Sedano <psedano@moneygram.com>
2016-07-20 17:07:03 -0500

    Initial Commit

---
+++ CoolPlaces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoolPlaces
+//
+//  Created by Pablo Sedano on 7/20/16.
+//  Copyright © 2016 Pablo Sedano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
